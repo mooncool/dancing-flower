a44f35571b06c339387f81abe7e77c48d8d9efe2
Moon <moon.ghost.jian@gmail.com>
2016-05-13 16:07:13 +0800

    Add build requirements

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var cssnano = require('gulp-cssnano');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var del = require('del');
 
 var paths = {
@@ -36,9 +39,19 @@ gulp.task('build:fonts', function() {
         .pipe(gulp.dest(paths.dist + '/fonts'));
 });
 
+gulp.task('build:js', function() {
+    return gulp.src(paths.fonts + '/**/*.js')
+        .pipe(concat('dancing-flower.js'))
+        .pipe(gulp.dest(paths.dist + '/js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.dist + '/js'));
+});
+
 gulp.task('build', [
     'build:css',
-    'build:fonts'
+    'build:fonts',
+    'build:js'
 ]);
 
 gulp.task('clean', function () {
